be78852d06873e781315efdd0a16e75b2a2d3dd7
liangsleep <1399353869@qq.com>
2025-03-05 16:52:42 +0800

    Feat:login and register

--- backend/oep/src/main/java/com/example/oep/exception/InvalidAuthCodeException.java
+++
@@ -1,7 +0,0 @@
-package com.example.oep.exception;
-
-public class InvalidAuthCodeException extends RuntimeException {
-    public InvalidAuthCodeException(String message) {
-        super(message);
-    }
-}
--- backend/oep/src/main/java/com/example/oep/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.oep.repository;
-
-import com.example.oep.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, String> {
-    boolean existsByStudentIdAndPassword(String username, String password);
-}
